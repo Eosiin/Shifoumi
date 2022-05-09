d6430515fa6829125b40961e0693cb035730e871
Eosiin <jordan.mabille76@outlook.fr>
2022-05-09 16:51:02 +0200

    Shifoumi

---
+++ exoCocktail/script.js
@@ -0,0 +1,10 @@
+// je cible l'id respectif
+
+let button = document.getElementById('button')
+let choix = document.getElementById('choix')
+
+
+
+function afficherPhrase(){
+    console.log('Vous avez choisi le mojito à 9€')
+}
